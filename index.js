3f44ae51e75903ffa780faa1b525db302ad1c7c6
Abby Lortz <abilenelortz03@gmail.com>
2018-06-20 20:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = 'Abby Lortz';
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortgreeting = '';
